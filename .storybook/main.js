c1b6260eb8bc16e9ac140924114c83511b6de401
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2023-06-11 18:14:06 +1200

    Switch to typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,26 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
+
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
+    autodocs: "tag"
   },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+      propFilter: () => true,
+    }
+  }
 };
+
 export default config;
